f863691c50af81729bfdb2e1b3598ded4f2a60c8
Stanley <stanleyobitunwase@gmail.com>
2024-04-15 14:03:38 +0100

    first commit

---
+++ car.java
@@ -0,0 +1,31 @@
+package repo10;
+
+public class car {
+    String make;
+    String model;
+    int year;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
